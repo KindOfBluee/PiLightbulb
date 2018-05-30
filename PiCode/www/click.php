564d3f8d49410c2d727d92f1c0d7f33b62128a56
KindOfBluee <81320622189a@gmail.com>
2018-05-30 22:11:41 +0200

    Dodane akcje na kliknięcie

--- PiCode/www/click.php
+++ PiCode/www/click.php
@@ -1,9 +1,8 @@
 <?php
-  if($_POST['action'] == 'call_this'){
-    if(isset($_POST['Desk'])){
-      print "1";
-    }else{
-      print "0";
-    }
+  if($_POST['action'] == 'desk'){
+    exec("sudo python3 /home/pi/PiLightbulb/desk.py");
+  }
+  if($_POST['action'] == 'bed'){
+    exec("sudo python3 /home/pi/PiLightbulb/desk.py");
   }
 ?>
